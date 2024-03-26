35a80e43a35a70d6b1049a9d59586d55de063e0c
panjiprann <panjipranantya20@gmail.com>
2024-03-26 14:26:59 +0700

    membuat penjumlahan

---
+++ pertemuan5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int bilangan1, bilangan2;
+
+void inputdata()
+{
+   cout << "masukkan bilangan pertama : ";
+   cin >> bilangan1;
+   cout << "masukkan bilangan kedua : ";
+   cin >> bilangan2;
+}
+
+int main(){
+    
+}
